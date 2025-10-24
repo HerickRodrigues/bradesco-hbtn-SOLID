1ba4fbe336ce0dd96ccb965b7cbf9f324af2683f
HerickRodrigues <herickcj@hotmail.com>
2025-10-24 15:28:12 -0300

    2. Convenções de Nomenclatura

---
+++ 0x04/convencao/UserModel.java
@@ -0,0 +1,15 @@
+// classe users, representando nossa entidade
+
+class UsersModels {
+
+int IdUser;
+
+String Descricao;
+
+final static int IDADEMINIMA = 18;
+
+// função que coleta todos os users
+
+public GetUsers() {return [];}
+
+}
